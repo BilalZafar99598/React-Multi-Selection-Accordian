198cb88b613bf6f707a769cb655bec059afcf032
BilalZafar99598 <bilal.halfur@gmail.com>
2024-03-04 18:23:19 +0500

    React Accordian App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Accordian from "./Accordian"
+
+const App = () => {
+
+
+  return (
+    <>
+      <Accordian/>
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, accusamus!',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae optio pariatur itaque accusamus perspiciatis ipsam hic quae a, iusto odio fugit perferendis expedita voluptates atque.'
+    },
+    {
+        id: '2',
+        question: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, accusamus!',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae optio pariatur itaque accusamus perspiciatis ipsam hic quae a, iusto odio fugit perferendis expedita voluptates atque.'
+    },
+    {
+        id: '3',
+        question: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, accusamus!',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae optio pariatur itaque accusamus perspiciatis ipsam hic quae a, iusto odio fugit perferendis expedita voluptates atque.'
+    },
+    {
+        id: '4',
+        question: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, accusamus!',
+        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae optio pariatur itaque accusamus perspiciatis ipsam hic quae a, iusto odio fugit perferendis expedita voluptates atque.'
+    }
+];
+
+export default data;
